ef6ad77baecf42f488d3be6acc287cb817e698a0
ryantaylorr <ryann.taylor@pm.me>
2024-08-10 13:45:59 -0400

    fixed spacing and loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,34 +1,29 @@
-use std::io::{self, Read};
+use std::io::{self};
 use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Hello User,");
-    println!("Guess the number!\n");
+    println!("-------------------------------\nHello User,");
+    println!("I need some information from you.\n");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut name = String::new();
+    let mut quest = String::new();
 
-loop{
-    loop{
-        let mut name = String::new();
-        let mut guess = String::new();
-        let mut answer = String::new();
-        let mut quest = String::new();
-
-        println!("\nWhat is your name?");
+        println!("-------------------------------\nWhat is your name?");
         io::stdin()
             .read_line(&mut name)
             .expect("Failed to read line");
-        println!("What is your quest?");
+        println!("-------------------------------\nWhat is your quest?");
             io::stdin()
             .read_line(&mut quest)
             .expect("Failed to read line");
-        println!("What is the airspeed velocity of an unladen swallow?\nJust Kidding, we're playing a guessing game.\nwhat is your guess?");
-            io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        println!("-------------------------------\nWhat is the airspeed velocity of an unladen swallow?\nJust Kidding, we're playing a guessing game.\n");
+        loop{
+        println!("What's your guess?");
+        let mut guess = String::new();
         io::stdin()
-            .read_line(&mut answer)
+            .read_line(&mut guess)
             .expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -41,11 +36,10 @@ loop{
             Ordering::Less => println!("Too small!\n"),
             Ordering::Greater => println!("Too big!\n"),
             Ordering::Equal => {
-                println!("\nThe Secret Number was: {secret_number}\nLet's Play Again.\n-------------------------------");
+                println!("-------------------------------\nThe Secret Number was: {secret_number}\nLet's Play Again.\n-------------------------------");
+                println!("to exit press Ctrl+C\n-------------------------------");
                 
-                break;
                 }
-            }
         }
     }
 }
